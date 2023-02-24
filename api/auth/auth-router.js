0050c5d21387b4d00f131b47c74e47db7fe7759a
Jamison Fox <fox.jamison@outlook.com>
2023-02-24 15:46:35 -0700

    /auth/login endpoint completed

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -76,8 +76,24 @@ router.post('/register', checkUsernameFree, checkPasswordLength, async (req, res
   }
 })
 
-router.post('/login', checkUsernameExists, (req, res, next) => { // eslint-disable-line
-  res.status(200).json({ message: 'working on post api/auth/login' })
+router.post('/login', checkUsernameExists, async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const [user] = await User.findBy({ username }) 
+    // I'm making this^^^ trip to the db in order to get the hashed password
+    // I could also use the checkUsernameExists middleware to append the hashed
+    // password to the req.
+    // That would keep my endpoint code cleaner and would maybe be a better use
+    // of middleware. 🤷‍♂️
+    if (user && bcrypt.compareSync(password, user.password)) {
+      req.session.user = user
+      res.json({ message: `Welcome ${user.username}!` })
+    } else {
+      next({ status: 401, message: 'Invalid credentials' })
+    }
+  } catch (err){
+    next(err)
+  }
 })
 
 router.get('/logout', (req, res, next) => { // eslint-disable-line
