3f70b1bc0e65555aa90692530ee36af0d11cf46f
Jamison Fox <fox.jamison@outlook.com>
2023-02-24 14:06:49 -0700

    all middleware complete

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,31 +1,10 @@
 const User = require('../users/users-model')
 
-/*
-  If the user does not have a session saved in the server
-
-  status 401
-  {
-    "message": "You shall not pass!"
-  }
-*/
 async function restricted(req, res, next) {
-  // if (req.session.user) {
-  //   next()
-  // } else{
-  //   next({ status: 401, message: 'you shall not pass!' })
-  // }
   console.log(`we've passed through the restricted middleware`)
   next()
 }
 
-/*
-  If the username in req.body already exists in the database
-
-  status 422
-  {
-    "message": "Username taken"
-  }
-*/
 async function checkUsernameFree(req, res, next) {
   try{
     const users = await User.findBy({ username: req.body.username })
@@ -40,14 +19,6 @@ async function checkUsernameFree(req, res, next) {
   }
 }
 
-/*
-  If the username in req.body does NOT exist in the database
-
-  status 401
-  {
-    "message": "Invalid credentials"
-  }
-*/
 async function checkUsernameExists(req, res, next) {
   try{
     const users = await User.findBy({ username: req.body.username })
@@ -62,20 +33,15 @@ async function checkUsernameExists(req, res, next) {
   }
 }
 
-/*
-  If password is missing from req.body, or if it's 3 chars or shorter
-
-  status 422
-  {
-    "message": "Password must be longer than 3 chars"
-  }
-*/
 function checkPasswordLength(req, res, next) {
   console.log(`we've passed through the checkPasswordLength middleware`)
-  next()
+  if (req.body.password === undefined || req.body.password.trim().length < 3) {
+    next({ status: 422, message: 'Password must be longer than 3 chars' })
+  } else {
+    next()
+  }
 }
 
-// Don't forget to add these to the `exports` object so they can be required in other modules
 module.exports = {
   restricted,
   checkUsernameFree,
